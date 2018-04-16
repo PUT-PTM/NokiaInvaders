09859f3c704caa2f09bf23ac97d7aaae88172f68
ChrisKru <christopherkru@gmail.com>
2018-04-16 20:04:01 +0200

    square control

--- src/main.c
+++ src/main.c
@@ -2,16 +2,20 @@
 #include "stm32f4_discovery.h"
 #include "stm32f4xx_syscfg.h"
 #include "tm_stm32f4_lis302dl_lis3dsh.h"
+#include "tm_stm32f4_pcd8544.h"
 
 typedef enum{
 	false, true
 } bool;
 
-int16_t X;
-int16_t Y;
-int16_t Z;
+int x_pos;
+int y_pos;
 
 int main(void){
+	//pcd8544
+	SystemInit();
+	PCD8544_Init(0x38);
+	//diodes
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	GPIO_InitTypeDef GPIO_diodes;
 	GPIO_diodes.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
@@ -20,24 +24,43 @@ int main(void){
 	GPIO_diodes.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_diodes.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_diodes);
-
-	//accelerometer control
+	//accelerometer
 	TM_LIS302DL_LIS3DSH_Init(TM_LIS3DSH_Sensitivity_2G, TM_LIS3DSH_Filter_800Hz);
 	TM_LIS302DL_LIS3DSH_t axes;
+
+	int X, Y, Z;
+	int width = 10;
+	int height = 10;
+	x_pos = 84/2 - width/2;
+	y_pos = 48 - height;
+	bool movement = false;
+	PCD8544_DrawFilledRectangle(x_pos, y_pos, x_pos+width, y_pos+height, 1);
+	PCD8544_Refresh();
 	for(;;){
 		TM_LIS302DL_LIS3DSH_ReadAxes(&axes);
 		X = axes.X * 0.06;
 		Y = axes.Y * 0.06;
 		Z = axes.Z * 0.06;
 		if(X > 250){
-			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
-			GPIO_SetBits(GPIOD, GPIO_Pin_14);
+			movement = true;
+			--x_pos;
 		}else if(X < -250){
-			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
-			GPIO_SetBits(GPIOD, GPIO_Pin_12);
-		}else{
-			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
-			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+			movement = true;
+			++x_pos;
+		}
+		if(movement){
+			movement = false;
+			if(x_pos < 3){
+				x_pos = 3;
+			}
+			if(x_pos + width > 80){
+				x_pos = 80 - width;
+			}
+			PCD8544_Clear();
+			PCD8544_DrawFilledRectangle(x_pos, y_pos, x_pos+width, y_pos+height, 1);
+			PCD8544_Refresh();
 		}
 	}
+	//180MHz
+
 }
